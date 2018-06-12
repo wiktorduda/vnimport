34584963d849b5d0189cf2380f079c516d511559
Wiktor Duda <wiktorduda@protonmail.com>
2018-06-12 16:38:26 +0800

    complete initial version

---
+++ vnimport/playnitemodel.py
@@ -0,0 +1,43 @@
+import System
+
+class UpdateGameModel:
+	def __init__(self, playnite, is_roman=False):
+		self.playnite = playnite
+		self.is_roman = is_roman
+
+	def update_all(self, game_obj, model):
+		self.update_name(game_obj, model)
+		self.update_developers(game_obj, model)
+		self.update_release_date(game_obj, model)
+		self.update_links(game_obj, model)
+		self.update_cover_image(game_obj, model)
+		
+	def update_name(self, game_obj, model):
+		if self.is_roman:
+			self.update_roman_name(game_obj, model)
+		else:
+			self.update_kanji_name(game_obj, model)
+
+	def update_kanji_name(self, game_obj, model):
+		name = game_obj['kanji_name']
+		model.Name = name
+
+	def update_roman_name(self, game_obj, model):
+		roman_name = game_obj['roman_name']
+		model.Name = roman_name
+
+	def update_developers(self, game_obj, model):
+		developers = game_obj['developers']
+		model.Developers = System.Collections.Generic.ComparableList[str](developers)
+		
+	def update_release_date(self, game_obj, model):
+		release_date = game_obj['release_date']
+		model.ReleaseDate = System.DateTime.ParseExact(str(release_date), 'yyyyMMdd', None)
+
+	def update_links(self, game_obj, model):
+		links = game_obj['links']
+		model.Links = System.Collections.ObjectModel.ObservableCollection[self.playnite.SDK.Models.Link](links)
+	
+	def update_cover_image(self, game_obj, model):
+		if 'image_id' in game_obj:
+			model.Image = game_obj['image_id']
---
+++ vnimport/resource.py
@@ -0,0 +1,61 @@
+import urllib
+import urllib2
+import os
+import json
+import tempfile
+import uuid
+import time
+
+rate = 2.0
+per_second = 5.0
+
+def rate_limit(rate, per_second):
+	if rate < 1.0:
+		rate = 1.0 / rate
+		per_second = per_second * (rate * rate)
+	def decorate(func):
+		last_check = time.time()
+		def rate_limit_func(*args,**kargs):
+			allowance = rate
+			elapsed = time.time() - last_check
+			allowance += elapsed * (rate / per_second)
+			if allowance > rate:
+				allowance = rate
+			if allowance < 1.0:
+				sleep_time = (1.0 - allowance) * (per_second / rate)
+				time.sleep(sleep_time)
+			allowance -= 1.0
+			return func(*args,**kargs)
+		return rate_limit_func
+	return decorate
+
+@rate_limit(rate, per_second)
+def search_erogetrailers(keyword):
+	payload = {
+		'md': 'search_game',
+		'sw': keyword.encode('utf-8')
+	}
+	url_payload = urllib.urlencode(payload)
+	url = 'http://erogetrailers.com/api?{}'
+	r = urllib2.urlopen(url.format(url_payload))
+	return json.loads(r.read().decode('utf-8'))
+
+@rate_limit(rate, per_second)
+def dowload_getchu_cover_image(getchu_id):
+	return DownloadGetchuCoverImage(getchu_id)
+
+class DownloadGetchuCoverImage:
+    def __init__(self, getchu_id):
+        self.getchu_id = getchu_id
+	
+    def __enter__(self):
+		id = str(uuid.uuid4())
+		filename = os.path.join(tempfile.gettempdir(), '{}.jpg'.format(id))
+		url = 'http://www.getchu.com/brandnew/{0}/rc{0}package.jpg'
+		with open(filename, 'wb') as f:
+			f.write(urllib2.urlopen(url.format(self.getchu_id)).read())
+		self.filename = filename
+		return (id, filename)
+
+    def __exit__(self, type, value, traceback):
+        os.remove(self.filename)
